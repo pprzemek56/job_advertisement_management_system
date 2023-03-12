226a84bbebec5b83f22115f6bbafb051c3e45cac
pprzemek56 <przemkopis@wp.pl>
2023-03-12 02:25:45 +0100

    wip creat user models

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+
+class JobSeeker(AbstractBaseUser):
+    """
+    Single app user
+    """
+    name = models.CharField(max_length=30)
+    surname = models.CharField(max_length=30)
+    phone_number = models.CharField(max_length=9, null=True, unique=True)
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = "email"
+
+
+class Company(AbstractBaseUser):
+    """
+    Company app user
+    """
+    company_name = models.CharField(max_length=200)
+    email = models.EmailField(unique=True)
+    nip_number = models.CharField(max_length=13, unique=True)
+
+    USERNAME_FIELD = "email"
