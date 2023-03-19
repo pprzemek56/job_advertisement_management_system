25a64eabae1a7a179a8f93216654c04590874832
pprzemek56 <przemkopis@wp.pl>
2023-03-19 16:06:11 +0100

    create register job seeker user form

--- src/accounts/forms.py
+++ src/accounts/forms.py
@@ -50,7 +50,83 @@ class RegisterCompanyForm(forms.ModelForm):
         """
         cleaned_data = super().clean()
         password = cleaned_data.get("password")
-        if password != self.password_2:
+        password_2 = cleaned_data.get("password_2")
+        if password != password_2:
+            raise forms.ValidationError("passwords doesn't match")
+
+        return cleaned_data
+
+
+class RegisterJobSeekerForm(forms.ModelForm):
+    """
+    Register job seeker
+    """
+
+    password_2 = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = JobSeeker
+        fields = [
+            "email",
+            "first_name",
+            "last_name",
+            "phone_number",
+            "cv",
+            "password"
+        ]
+
+    def clean_email(self):
+        """
+        verify email is available
+        """
+        email = self.cleaned_data.get("email")
+        if Company.objects.filter(email=email).exists() \
+                or JobSeeker.objects.filter(email=email).exists():
+            raise forms.ValidationError("email is taken")
+
+        return email
+
+    def clean_first_name(self):
+        """
+        verify first name is valid
+        """
+        first_name = self.cleaned_data.get("first_name")
+        if re.match(r"^[a-zA-Z]+$", first_name) is None:
+            raise forms.ValidationError("First name is invalid")
+
+        return first_name
+
+    def clean_last_name(self):
+        """
+        verify last name is valid
+        """
+        last_name = self.cleaned_data.get("last_name")
+        if re.match(r"^[a-zA-Z]+$", last_name) is None:
+            raise forms.ValidationError("Last name is invalid")
+
+        return last_name
+
+    def clean_phone_number(self):
+        """
+        verify nip number is valid and available
+        """
+        phone_number = self.cleaned_data.get("phone_number")
+        if JobSeeker.objects.filter(phone_number=phone_number).exists():
+            raise forms.ValidationError("nip number is taken")
+
+        if re.match(r"^\d{9}$") is None:
+            raise forms.ValidationError("nip number is invalid")
+
+        return phone_number
+
+    def clean(self):
+        """
+        verify passwords are the same
+        """
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        password_2 = cleaned_data.get("password_2")
+        if password != password_2:
             raise forms.ValidationError("passwords doesn't match")
 
         return cleaned_data
