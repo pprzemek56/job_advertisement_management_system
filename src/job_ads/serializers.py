6fb284d998096e852f416aba133e078fa6c10f36
pprzemek56 <przemkopis@wp.pl>
2023-03-22 01:03:03 +0100

    create position serializer

---
+++ src/job_ads/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework.serializers import ModelSerializer
+
+from .models import Position
+
+
+class PositionSerializer(ModelSerializer):
+    class Meta:
+        model = Position
+        fields = [
+            "id",
+            "name"
+        ]
+
+
